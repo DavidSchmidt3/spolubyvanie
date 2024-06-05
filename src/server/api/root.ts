d15be63e227c3b0d8a8ff42af4d7be6b22fbe987
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-05 12:14:17 +0200

    feat(trpc): types

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { userSettingsRouter } from "@/server/api/routers/user_settings";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 
 /**
  * This is the primary router for your server.
@@ -12,7 +13,8 @@ export const appRouter = createTRPCRouter({
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
-
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
 /**
  * Create a server-side caller for the tRPC API.
  * @example
--- src/server/api/routers/user_settings.ts
+++ src/server/api/routers/user_settings.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { type RouterOutput } from "../root";
 
+export type UserSettings = RouterOutput["userSettings"]["get"];
 export const userSettingsRouter = createTRPCRouter({
   get: publicProcedure.input(z.string()).query(({ input, ctx }) => {
     return ctx.db.user_settings.findFirst({
       where: {
-        user_id: input,
+        id: input,
       },
     });
   }),
