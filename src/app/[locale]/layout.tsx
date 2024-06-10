59805845a5697297ae9cf1b925f0e1db52c22368
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-10 21:42:03 +0200

    feat(layout): add toaster, styling

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,12 @@
 import SuppressLogs from "@/app/[locale]/_components/common/suppress-logs";
 import { MainNav } from "@/app/[locale]/_components/layouts/main-nav";
 import { UserNav } from "@/app/[locale]/_components/layouts/user-nav";
+import { ThemeProvider } from "@/app/[locale]/_components/theme-provider";
+import { Toaster } from "@/app/[locale]/_components/ui/toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { TRPCReactProvider } from "@/trpc/react";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "./_components/theme-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -35,12 +36,13 @@ export default function RootLayout({
       >
         <TRPCReactProvider>
           <ThemeProvider>
-            <div className="flex items-center px-4 min bg-background py-4 min-h-20 border-b w-full sticky top-0 z-20">
+            <div className="sticky top-0 z-20 flex items-center w-full px-4 py-4 border-b min bg-background min-h-20">
               <MainNav className="mx-6" />
-              <div className="ml-auto flex items-center space-x-4">
+              <div className="flex items-center ml-auto space-x-4">
                 <UserNav />
                 {/* // TODO: - delete when https://github.com/radix-ui/primitives/pull/2811 gets merged  */}
                 <SuppressLogs />
+                <Toaster />
               </div>
             </div>
             {children}
