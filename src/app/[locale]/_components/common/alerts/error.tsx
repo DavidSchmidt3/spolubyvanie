0d47e963dece82dfce2999d99e4c32f9fa6a888c
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-10 21:44:15 +0200

    feat(error-alert): adapt to trpc instead of supabase

--- src/app/[locale]/_components/common/alerts/error.tsx
+++ src/app/[locale]/_components/common/alerts/error.tsx
@@ -5,28 +5,28 @@ import {
   AlertTitle,
 } from "@/app/[locale]/_components/ui/alert";
 import { cn } from "@/lib/utils";
-import { ApiError } from "@/lib/utils/supabase/error";
+import { type AppRouter } from "@/server/api/root";
+import { type TRPCClientErrorLike } from "@trpc/client";
 import { useTranslations } from "next-intl";
 
 interface Props {
-  error: Error;
+  error: Error | TRPCClientErrorLike<AppRouter>;
   className?: string;
 }
 
-const NS = "validation";
-
-export default function ErrorAlert({ error, className }: Props) {
+// no destructuring because otherwise next ts plugin complains that error is not serializable in client component
+// fix would be to not mark as client component, however that triggers next.js error that error components must be client components
+export const ErrorAlert = (props: Props) => {
   const t = useTranslations();
 
-  let errorCode = "unexpected_error";
-  if (error instanceof ApiError) {
-    errorCode = `${error.name}.${error.code}`;
-  }
-
   return (
-    <Alert variant="destructive" className={cn("mt-2", className)}>
+    <Alert variant="destructive" className={cn("mt-2", props.className)}>
       <AlertTitle>{t("common.error.label")}</AlertTitle>
-      <AlertDescription>{t(errorCode, { ns: NS })}</AlertDescription>
+      {!(props.error instanceof Error) ? (
+        <AlertDescription>
+          {t(`errors.${props.error.data?.code}`)}
+        </AlertDescription>
+      ) : null}
     </Alert>
   );
-}
+};
