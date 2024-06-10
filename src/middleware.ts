986c4c7154e7cd4dbb77fbe7a26fdd4eb1bc199f
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-10 21:41:30 +0200

    feat(middleware): handle localization, supabase update cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,10 @@ import {
   LOCALES_CODES,
   pathnames,
 } from "@/lib/utils/localization/i18n";
-import { updateSession } from "@/lib/utils/supabase/middleware";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import createMiddleware from "next-intl/middleware";
 import { type NextRequest } from "next/server";
+import { type Database } from "./lib/utils/supabase/types/database";
 
 const localizationMiddleWare = createMiddleware({
   defaultLocale: DEFAULT_LOCALE,
@@ -16,13 +17,36 @@ const localizationMiddleWare = createMiddleware({
 });
 
 export async function middleware(request: NextRequest) {
-  if (supabaseRoutes.includes(request.nextUrl.pathname)) {
-    await updateSession(request);
-  }
+  const response = localizationMiddleWare(request);
+
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          request.cookies.set({ name, value, ...options });
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          response.cookies.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          request.cookies.set({ name, value: "", ...options });
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          response.cookies.set({ name, value: "", ...options });
+        },
+      },
+    }
+  );
+
+  await supabase.auth.getUser();
+
   return localizationMiddleWare(request);
 }
-
-const supabaseRoutes = ["settings"] as string[];
 
 export const config = {
   // Match only internationalized pathnames
