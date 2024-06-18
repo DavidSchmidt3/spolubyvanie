b4fa97a71d06b03f92ea352c3d89d7228a533228
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-18 21:38:18 +0200

    feat(next-config): security headers

--- next.config.js
+++ next.config.js
@@ -8,12 +8,53 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin(
   "./src/lib/utils/localization/i18n.ts"
 );
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    `;
 
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
     ppr: true,
     reactCompiler: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)?",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
   },
 };
 
