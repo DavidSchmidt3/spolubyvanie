6357bc8b5e5efda4657dd2a2171ae0d8d6175b0d
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-19 16:16:31 +0200

    refactor(password-change): use next safe action

--- src/lib/utils/data/actions/password-change/index.ts
+++ src/lib/utils/data/actions/password-change/index.ts
@@ -1,46 +1,38 @@
 "use server";
 
+import {
+  ActionError,
+  actionClient,
+} from "@/lib/utils/data/actions/safe-action-client";
 import { createClient } from "@/lib/utils/supabase/server";
-import { formatZodErrorsToArray } from "@/lib/utils/zod";
+import { formatZodErrors } from "@/lib/utils/zod";
 import { PASSWORD_CHANGE_SCHEMA } from "./schema";
 import { getTranslatedSupabasePasswordChangeError } from "./supabase-password-change-errors";
 
-export async function changePassword(input: unknown) {
-  const supabase = createClient();
-  const validatedChangePasswordInput = PASSWORD_CHANGE_SCHEMA.safeParse(input);
+export const changePassword = actionClient
+  .schema(PASSWORD_CHANGE_SCHEMA, {
+    handleValidationErrorsShape: formatZodErrors,
+  })
+  .action(async ({ parsedInput: { password, accessToken } }) => {
+    const supabase = createClient();
 
-  if (!validatedChangePasswordInput.success) {
-    const errors = formatZodErrorsToArray(validatedChangePasswordInput);
-    console.error("Error validating reset password input", errors.flat());
-    return {
-      isError: true,
-      error: errors,
-    };
-  }
+    try {
+      await supabase.auth.exchangeCodeForSession(accessToken);
+    } catch (error) {
+      throw new ActionError("alerts.password_change.access_token.invalid");
+    }
 
-  try {
-    await supabase.auth.exchangeCodeForSession(
-      validatedChangePasswordInput.data.accessToken
-    );
-  } catch (error) {
-    console.error("Error exchanging access token", error);
-    return {
-      isError: true,
-      error: "alerts.password_change.access_token.invalid" as const,
-    };
-  }
+    const { error } = await supabase.auth.updateUser({
+      password,
+    });
 
-  const { error } = await supabase.auth.updateUser({
-    password: validatedChangePasswordInput.data.password,
+    // never trust session data after one use, we want user to sign with the new password anyway
+    await supabase.auth.signOut();
+
+    if (error) {
+      console.error("Error resetting password with email", error);
+      throw new ActionError(
+        getTranslatedSupabasePasswordChangeError(error.message)
+      );
+    }
   });
-
-  await supabase.auth.signOut();
-
-  if (error) {
-    console.error("Error resetting password with email", error);
-    return {
-      isError: true,
-      error: getTranslatedSupabasePasswordChangeError(error.message),
-    };
-  }
-}
