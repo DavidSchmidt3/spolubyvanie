d87e52d37bcf227dd3155538037715258641616a
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-21 08:59:54 +0200

    feat(error-page): make it static

--- src/app/[locale]/error/page.tsx
+++ src/app/[locale]/error/page.tsx
@@ -1,7 +1,39 @@
 import Container from "@/app/[locale]/_components/common/container";
+import { LOCALES, type Locale } from "@/lib/utils/localization/i18n";
+import { Link } from "@/lib/utils/localization/navigation";
 import { useTranslations } from "next-intl";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 
-export default function ErrorPage() {
+type Props = {
+  params: {
+    locale: Locale;
+  };
+};
+
+export const dynamic = "force-static";
+export default function ErrorPage({ params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
   const t = useTranslations("translations");
-  return <Container>{t("common.error.label")}</Container>;
+  return (
+    <Container className="text-center">
+      <h1 className="text-2xl">{t("common.error.label")}</h1>
+      <div className="mt-5"></div>
+      <Link href="/" className="underline hover:text-primary text-xl">
+        {t("common.error.redirect_home")}
+      </Link>
+    </Container>
+  );
+}
+
+export async function generateMetadata({ params: { locale } }: Props) {
+  const t = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: t("error.title"),
+    description: t("error.description"),
+  };
+}
+
+export function generateStaticParams() {
+  return LOCALES.map((locale) => ({ locale: locale.code }));
 }
