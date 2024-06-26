ddb5d141b89c9dad83c78fed4ea9b16d83a1e9b4
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-26 19:14:23 +0200

    feat(seo): add test sitemap, robots

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { type MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/auth", "/password-reset", "/password-change"],
+    },
+    sitemap: "https://spolubyvanie.vercel.app/sitemap.xml",
+  };
+}
