659ecaf6a7c1dfeedc55a5b9ea104d6c60160e24
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-06-28 15:51:48 +0200

    Filters form (#13)
    * feat(database): fetch schema
    
    * refactor(data): move to folders
    
    * feat(advertisement-filter): styling, collapsible card
    
    * fix(i18n): wrong key, type
    
    * feat(robots): disallow page
    
    * refactor(advertisement-filter): move to modal

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,18 @@
-import Container from "@/app/[locale]/_components/common/container";
 import AdministrativeDivisionFilter from "@/app/[locale]/_components/home/administrative-division-filter";
 import { AdvertisementTypeFilter } from "@/app/[locale]/_components/home/advertisement-type-filter";
-import CollapsibleWrapper from "@/app/[locale]/_components/home/collapsible-wrapper";
 import PriceFilter from "@/app/[locale]/_components/home/price-filter";
 import SubmitButton from "@/app/[locale]/_components/home/submit-button";
-import { Card } from "@/app/[locale]/_components/ui/card";
+import { Button } from "@/app/[locale]/_components/ui/button";
+import { Card, CardContent } from "@/app/[locale]/_components/ui/card";
+import {
+  Credenza,
+  CredenzaBody,
+  CredenzaContent,
+  CredenzaHeader,
+  CredenzaTitle,
+  CredenzaTrigger,
+} from "@/app/[locale]/_components/ui/credenza";
+import { Icons } from "@/app/[locale]/_components/ui/icons";
 import {
   getDistricts,
   getMunicipalities,
@@ -24,29 +32,52 @@ export default async function Home() {
   const t = await getTranslations("translations");
 
   return (
-    <Container fullWidth className="flex justify-center sm:py-0">
-      <NextIntlClientProvider
-        messages={pickLocaleMessages(messages, ["translations.advertisement"])}
-      >
-        <div className="flex justify-center">
-          <Card className="mx-4 sm:mx-10 max-w-2xl w-full">
-            <CollapsibleWrapper title={t("advertisement.filter.title")}>
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <AdministrativeDivisionFilter
-                  regions={regions}
-                  districts={districts}
-                  municipalities={municipalities}
-                />
-                <div className="flex flex-col gap-y-2 gap-x-4 sm:gap-x-8">
-                  <PriceFilter />
-                  <AdvertisementTypeFilter />
+    <NextIntlClientProvider
+      messages={pickLocaleMessages(messages, ["translations.advertisement"])}
+    >
+      <Card className="rounded-none">
+        <CardContent className="py-5 px-5 sm:px-10 flex flex-col sm:flex-row justify-between gap-y-2 gap-x-4 items-center">
+          <div className="flex flex-col gap-y-4">
+            <h1 className="text-xl sm:text-3xl font-bold">
+              {t("advertisement.title")}
+            </h1>
+            <h2 className="text-md sm:text-lg">
+              {t("advertisement.description")}
+            </h2>
+          </div>
+          <Credenza>
+            <CredenzaTrigger asChild className="px-4 py-2 sm:py-2">
+              <Button
+                className="text-md flex gap-2 text-wrap h-14 w-40"
+                variant="outline"
+              >
+                <Icons.filter className="w-8 h-8" />
+                {t("advertisement.filter.title")}
+              </Button>
+            </CredenzaTrigger>
+            <CredenzaContent className="max-w-3xl pb-8 sm:pb-0">
+              <CredenzaHeader>
+                <CredenzaTitle>{t("advertisement.filter.title")}</CredenzaTitle>
+              </CredenzaHeader>
+              <CredenzaBody>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-8">
+                  <AdministrativeDivisionFilter
+                    regions={regions}
+                    districts={districts}
+                    municipalities={municipalities}
+                  />
+                  <div className="flex flex-col gap-y-2 gap-x-4 sm:gap-x-8">
+                    <PriceFilter />
+                    <AdvertisementTypeFilter />
+                  </div>
+                  <SubmitButton />
                 </div>
-                <SubmitButton />
-              </div>
-            </CollapsibleWrapper>
-          </Card>
-        </div>
-      </NextIntlClientProvider>
-    </Container>
+              </CredenzaBody>
+            </CredenzaContent>
+          </Credenza>
+        </CardContent>
+      </Card>
+      <div className="mt-4 text-center">Tu budú ponuky</div>
+    </NextIntlClientProvider>
   );
 }
---
+++ src/hooks/media-query.ts
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { useEffect } from "react";
+
+export function useMediaQuery(query: string) {
+  const [value, setValue] = React.useState(false);
+
+  useEffect(() => {
+    function onChange(event: MediaQueryListEvent) {
+      setValue(event.matches);
+    }
+
+    const result = matchMedia(query);
+    result.addEventListener("change", onChange);
+    setValue(result.matches);
+
+    return () => result.removeEventListener("change", onChange);
+  }, [query]);
+
+  return value;
+}
