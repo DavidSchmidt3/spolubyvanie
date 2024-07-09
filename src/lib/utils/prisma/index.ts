5d3bfed32866c2a899dbbd185de40039ae88eb92
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-07-09 12:13:04 +0200

    feat(prisma): add pagination extension

--- src/lib/utils/prisma/index.ts
+++ src/lib/utils/prisma/index.ts
@@ -1,11 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { createPaginator } from "prisma-extension-pagination";
 
 import { env } from "@/env";
+
+const paginate = createPaginator({
+  pages: {
+    limit: 10, // set default limit to 10
+    includePageCount: true, // include counters by default
+  },
+});
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.VERCEL_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  }).$extends({
+    model: {
+      advertisements: {
+        paginate,
+      },
+    },
   });
 
 const globalForPrisma = globalThis as unknown as {
