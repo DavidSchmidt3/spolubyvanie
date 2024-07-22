5cc0e6ce1b1bb2cd002eeec0cdf9dde5ed1c0fb4
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-07-22 12:55:10 +0200

    feat(utils): compress, crop files

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,24 +1,91 @@
 import { clsx, type ClassValue } from "clsx";
+import Compressor from "compressorjs";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatBytes(
-  bytes: number,
-  opts: {
-    decimals?: number;
-    sizeType?: "accurate" | "normal";
-  } = {}
-) {
-  const { decimals = 0, sizeType = "normal" } = opts;
+export function compressFile(
+  file: File,
+  compressionQuality: number
+): Promise<File> {
+  return new Promise<File>((resolve) => {
+    new Compressor(file, {
+      quality: compressionQuality,
+      success: (compressedFile) => {
+        if (compressedFile instanceof Blob) {
+          const convertedCompressedFile = new File(
+            [compressedFile],
+            file.name,
+            {
+              type: compressedFile.type,
+            }
+          );
+          resolve(convertedCompressedFile);
+        } else {
+          resolve(compressedFile);
+        }
+      },
+      error: (error) => {
+        console.error("Error compressing file", error);
+        resolve(file);
+      },
+    });
+  });
+}
 
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-  const accurateSizes = ["Bytes", "KiB", "MiB", "GiB", "TiB"];
-  if (bytes === 0) return "0 Byte";
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
-    sizeType === "accurate" ? accurateSizes[i] ?? "Bytest" : sizes[i] ?? "Bytes"
-  }`;
+export function crop(file: File, aspectRatio: number): Promise<File> {
+  // we return a Promise that gets resolved with our canvas element
+  return new Promise((resolve) => {
+    // this image will hold our source image data
+    const inputImage = new Image();
+
+    // we want to wait for our image to load
+    inputImage.onload = () => {
+      // let's store the width and height of our image
+      const inputWidth = inputImage.naturalWidth;
+      const inputHeight = inputImage.naturalHeight;
+
+      // get the aspect ratio of the input image
+      const inputImageAspectRatio = inputWidth / inputHeight;
+
+      // if it's bigger than our target aspect ratio
+      let outputWidth = inputWidth;
+      let outputHeight = inputHeight;
+      if (inputImageAspectRatio > aspectRatio) {
+        outputWidth = inputHeight * aspectRatio;
+      } else if (inputImageAspectRatio < aspectRatio) {
+        outputHeight = inputWidth / aspectRatio;
+      }
+
+      // calculate the position to draw the image at
+      const outputX = (outputWidth - inputWidth) * 0.5;
+      const outputY = (outputHeight - inputHeight) * 0.5;
+
+      // create a canvas that will present the output image
+      const outputImage = document.createElement("canvas");
+
+      // set it to the same size as the image
+      outputImage.width = outputWidth;
+      outputImage.height = outputHeight;
+
+      // draw our image at position 0, 0 on the canvas
+      const ctx = outputImage.getContext("2d");
+      ctx?.drawImage(inputImage, outputX, outputY);
+      outputImage.toBlob((blob) => {
+        if (blob) {
+          // create a new File from the Blob with the same name and type as the original file
+          const croppedFile = new File([blob], file.name, { type: file.type });
+          resolve(croppedFile);
+        } else {
+          resolve(file);
+        }
+      }, file.type);
+      URL.revokeObjectURL(inputImage.src);
+    };
+
+    // start loading our image
+    inputImage.src = URL.createObjectURL(file);
+  });
 }
