31ce4a466e06a7688b8bf5a19cf2ec9cdaba40c9
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-07-25 09:38:46 +0200

    fix(transition-link): prevent blank background forever on navigating the same route

--- src/app/[locale]/_components/common/transition-link.tsx
+++ src/app/[locale]/_components/common/transition-link.tsx
@@ -7,7 +7,7 @@ import {
   useRouter,
 } from "@/lib/utils/localization/navigation";
 import { useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 type TransitionLinkProps<Pathname extends keyof typeof pathnames> = {
   children: React.ReactNode;
@@ -23,20 +23,41 @@ export const TransitionLink = <Pathname extends keyof typeof pathnames>({
 }: TransitionLinkProps<Pathname>) => {
   const router = useRouter();
   const pathname = usePathname();
+  const [params, setParams] = useState([] as string[]);
+
   const searchParams = useSearchParams();
   const searchParamsEntries = searchParams.entries();
+
+  // We need to prevent redirecting to the same page, because the useeffect watching for pathname or params changes
+  // will never trigger and transition background will stay on forever
+  function newRouteMatchesCurrentRoute() {
+    let isEqual = false;
+    if (typeof href === "string") {
+      isEqual = href === pathname;
+    } else if (typeof href === "object") {
+      isEqual = href.pathname === pathname;
+
+      if (isEqual && "params" in href) {
+        const hrefParams = Object.values(href.params);
+        isEqual = params.every((param, index) => param === hrefParams[index]);
+      }
+    }
+    return isEqual;
+  }
 
   async function handleTransition(
     e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) {
     e.preventDefault();
-    if (href === pathname) return;
+
+    if (newRouteMatchesCurrentRoute()) return;
     await pushRouteWithTransition(href, router);
   }
 
   useEffect(() => {
     const body = document.querySelector("body");
     body?.classList.remove("page-transition");
+    setParams(window.location.pathname.split("/").slice(2));
   }, [pathname, searchParamsEntries]);
 
   return (
