4e57125604e6d07d15a2e391f99f34889110efea
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-09-29 21:24:34 +0200

    fix(advertisement): styles

--- src/app/[locale]/_components/advertisement/info-card.tsx
+++ src/app/[locale]/_components/advertisement/info-card.tsx
@@ -33,55 +33,65 @@ export default function InfoCard({ advertisement, locale, properties }: Props) {
   } = advertisement;
 
   return (
-    <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 md:gap-10">
-      <div className="flex flex-col gap-y-4">
-        <InfoRow
-          label={t("advertisement.type.label")}
-          value={t(
-            `advertisement.types.${adTypeKeys[type as keyof typeof adTypeKeys]}`
+    <>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 md:gap-10">
+        <div className="flex flex-col gap-y-4">
+          <InfoRow
+            label={t("advertisement.type.label")}
+            value={t(
+              `advertisement.types.${
+                adTypeKeys[type as keyof typeof adTypeKeys]
+              }`
+            )}
+          />
+          <InfoRow
+            label={t("advertisement.price.label")}
+            value={`${price} €`}
+          />
+          {type === AdType.OfferingRoom && (
+            <InfoRow label={t("advertisement.street.label")} value={street} />
           )}
-        />
-        <InfoRow label={t("advertisement.price.label")} value={`${price} €`} />
-        {type === AdType.OfferingRoom && (
-          <InfoRow label={t("advertisement.street.label")} value={street} />
-        )}
-        <InfoRow
-          label={t("advertisement.municipality.title")}
-          value={municipality}
-        />
-        <InfoRow label={t("advertisement.district.title")} value={district} />
-        <InfoRow label={t("advertisement.region.title")} value={region} />
-        <InfoRow label={t("advertisement.views.title")} value={views} />
-      </div>
-      <div className="flex flex-col gap-y-4">
-        <InfoRow
-          label={t("advertisement.room_area.label")}
-          value={room_area ? `${room_area} m²` : null}
-        />
-        <InfoRow
-          label={t("advertisement.apartment_area.label")}
-          value={apartment_area ? `${apartment_area} m²` : null}
-        />
-        <InfoRow
-          label={t("advertisement.apartment_rooms.label")}
-          value={apartment_rooms ?? "-"}
-        />
-        <InfoRow label={t("advertisement.floor.label")} value={floor} />
-        <InfoRow label={t("advertisement.max_floor.label")} value={max_floor} />
-        <InfoRow
-          label={
-            type === AdType.OfferingRoom
-              ? t("advertisement.available_from.label")
-              : t("advertisement.searching_from.label")
-          }
-          value={formatDate(available_from, locale)}
-        />
+          <InfoRow
+            label={t("advertisement.municipality.title")}
+            value={municipality}
+          />
+          <InfoRow label={t("advertisement.district.title")} value={district} />
+          <InfoRow label={t("advertisement.region.title")} value={region} />
+          <InfoRow label={t("advertisement.views.title")} value={views} />
+        </div>
+        <div className="flex flex-col gap-y-4">
+          <InfoRow
+            label={t("advertisement.room_area.label")}
+            value={room_area ? `${room_area} m²` : null}
+          />
+          <InfoRow
+            label={t("advertisement.apartment_area.label")}
+            value={apartment_area ? `${apartment_area} m²` : null}
+          />
+          <InfoRow
+            label={t("advertisement.apartment_rooms.label")}
+            value={apartment_rooms ?? "-"}
+          />
+          <InfoRow label={t("advertisement.floor.label")} value={floor} />
+          <InfoRow
+            label={t("advertisement.max_floor.label")}
+            value={max_floor}
+          />
+          <InfoRow
+            label={
+              type === AdType.OfferingRoom
+                ? t("advertisement.available_from.label")
+                : t("advertisement.searching_from.label")
+            }
+            value={formatDate(available_from, locale)}
+          />
+        </div>
       </div>
       <Properties
         advertisementProperties={advertisement.advertisements_properties}
         locale={locale}
         properties={properties}
       />
-    </div>
+    </>
   );
 }
